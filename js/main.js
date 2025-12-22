b6343a53518315aff64edb8893a6b96a45f756ca
yuhyeon9808 <bwj9808@gmail.com>
2025-12-22 18:08:17 +0900

    feat header 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// 장바구니
+
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
 
@@ -13,3 +15,49 @@ basketEl.addEventListener('click', (e) => {
 window.addEventListener('click', () => {
   basketEl.classList.remove('show');
 });
+
+// 검색
+
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')];
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const documentEl = document.documentElement;
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+const showSearch = () => {
+  headerEl.classList.add('searching');
+  documentEl.classList.add('fixed');
+  headerMenuEls.reverse().forEach((el, idx) => {
+    el.style.transitionDelay = (idx * 0.4) / headerMenuEls.length + 's';
+  });
+
+  searchDelayEls.forEach((el, idx) => {
+    el.style.transitionDelay = (idx * 0.4) / searchDelayEls.length + 's';
+  });
+
+  setTimeout(() => {
+    searchInputEl.focus();
+  }, 600);
+};
+
+const hideSearch = () => {
+  headerEl.classList.remove('searching');
+  documentEl.classList.remove('fixed');
+  searchInputEl.value = '';
+  headerMenuEls.forEach((el, idx) => {
+    el.style.transitionDelay = (idx * 0.4) / headerMenuEls.length + 's';
+  });
+  searchDelayEls.reverse(el, idx) /
+    forEach(() => {
+      el.style.transitionDelay = (idx * 0.4) / searchDelayEls.length + 's';
+    });
+  searchDelayEls.reverse();
+};
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
